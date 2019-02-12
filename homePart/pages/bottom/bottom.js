5f08482b0b56901061d1ece741f38793d757150b
shrchang <liuchang_cloud@126.com>
2019-02-12 21:27:33 +0800

    初始化页面代码
    bottom是公共的引入，index是当前的指向，home是首页

---
+++ homePart/pages/bottom/bottom.js
@@ -0,0 +1,11 @@
+const app = getApp()
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    title: "底部"
+  },
+  onLoad: function () {
+
+  }
+})
---
+++ homePart/pages/home/home.js
@@ -0,0 +1,12 @@
+const app = getApp()
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    title:"首页",
+    index:1
+  },
+  onLoad: function () {
+    
+  }
+})
