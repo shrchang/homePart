cc959ba63e9c999720157e4f3c812ccd489fb602
liuchang <liuchang@fit2cloud.com>
2019-02-13 22:41:07 +0800

    首页修改；

--- homePart/pages/home/home.js
+++ homePart/pages/home/home.js
@@ -4,13 +4,21 @@ const util = require('../../utils/util.js')
 Page({
   data: {
     title:"首页",
-    index:1
+    index:1,
+    notice:"会员卡充500送50，充1200送120。",
+    rooms:[
+      { id: "a", src:"https://f11.baidu.com/it/u=1134393228,653228807&fm=72",text:"大学城轰趴基地a"},
+      { id: "b", src: "https://f12.baidu.com/it/u=1647895510,752235079&fm=72", text: "大学城轰趴基地b"},
+      { id: "c", src: "https://f12.baidu.com/it/u=1647895510,752235079&fm=72", text: "大学城轰趴基地c" },
+      { id: "d", src: "https://f12.baidu.com/it/u=1647895510,752235079&fm=72", text: "大学城轰趴基地d"},
+      { id: "e", src: "https://f12.baidu.com/it/u=1647895510,752235079&fm=72", text: "大学城轰趴基地e" }
+    ]
   },
   onLoad: function () {
     
   },
   //跳转到我的页面
-  selfBtn: function (options) {
+  selfBtn: function (e) {
     console.log(1111111)
     wx.switchTab({
       url: '../self/self'
