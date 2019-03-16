94f65086c9e1a39a23bf656e896b9650d6e84d50
Jianneng Pan <hub.0000147@live.com>
2019-03-16 17:27:07 +0800

    时间tab,显示今天及未来5天
    时间tab,显示今天及未来5天

--- homePart/pages/room/info/room_info.js
+++ homePart/pages/room/info/room_info.js
@@ -11,15 +11,36 @@ Component({
    * 组件的初始数据
    */
   data: {
+    bookedTabData: [], //预约tab数据
+    bookedDateData: [], //预约时间数据
+    packageData: [], //套餐数据
+    priceAmount: 0, //总金额
+    calendar: [],
     currentDateTab: 0,
     chooseSize: false,
     animationData: {},
-    radioItems: [
-      { name: 'USA', value: '场地基础套餐', disabled: true },
-      { name: 'CHN', value: '自助煮饭套餐(+250元)', checked: true },
-      { name: 'BRA', value: 'KTV套餐(+250元)' },
-      { name: 'JPN', value: '烧烤套餐(+300元)' },
-      { name: 'ENG', value: '体感游戏套餐(+150元)' }
+    radioItems: [{
+        name: 'USA',
+        value: '场地基础套餐',
+        disabled: true
+      },
+      {
+        name: 'CHN',
+        value: '自助煮饭套餐(+250元)',
+        checked: true
+      },
+      {
+        name: 'BRA',
+        value: 'KTV套餐(+250元)'
+      },
+      {
+        name: 'JPN',
+        value: '烧烤套餐(+300元)'
+      },
+      {
+        name: 'ENG',
+        value: '体感游戏套餐(+150元)'
+      }
     ],
     article: "我是html内容<br/>nihao, <br/><br/><br/><br/><br/><br/><br/>这个是测试<br/><br/>可以显示了<br/><br/><br/>hehe<b>很好，我变粗了</b >",
     xitem: {
@@ -89,6 +110,7 @@ Component({
       })
       let WxParse = require("../../wxParse/wxParse.js");
       WxParse.wxParse('article', 'html', this.data.article, this, 5);
+      this.getBookedTabData();
     },
     /**
      * 修改当前索引
@@ -140,7 +162,7 @@ Component({
       })
     },
     //显示预定页面
-    chooseSezi: function (e) {
+    chooseSezi: function(e) {
       // 用that取代this，防止不必要的情况发生
       var that = this;
       // 创建一个动画实例
@@ -162,7 +184,7 @@ Component({
         chooseSize: true
       })
       // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
-      setTimeout(function () {
+      setTimeout(function() {
         animation.translateY(10).step()
         that.setData({
           animationData: animation.export()
@@ -170,7 +192,7 @@ Component({
       }, 100)
     },
     //隐藏预定页面
-    hideModal: function (e) {
+    hideModal: function(e) {
       var that = this;
       var animation = wx.createAnimation({
         duration: 0,
@@ -182,7 +204,7 @@ Component({
         animationData: animation.export()
 
       })
-      setTimeout(function () {
+      setTimeout(function() {
         animation.translateY(10).step()
         that.setData({
           animationData: animation.export(),
@@ -191,7 +213,7 @@ Component({
       }, 200)
     },
     //点击切换
-    clickDateTab: function (e) {
+    clickDateTab: function(e) {
       var that = this;
       if (this.data.currentDateTab === e.currentTarget.dataset.current) {
         return false;
@@ -200,11 +222,73 @@ Component({
           currentDateTab: e.currentTarget.dataset.current
         })
       }
-    }
-    ,
+    },
     //弹出窗状态,阻止页面穿透操作
-    stopPageScroll:function (){
+    stopPageScroll: function() {
       return;
-    }
+    },
+    //每月第一天是星期几
+    getFirstDayOfWeek: function(year, month) {
+      return new Date(Date.UTC(year, month - 1, 1)).getDay();
+    },
+
+    //获取当前月的天数
+    getThisMonthDays: function() {
+      var date = new Date();
+      //将当前月份加1，下移到下一个月
+      date.setMonth(date.getMonth() + 1);
+      //将当前的日期置为0，
+      date.setDate(0);
+      //再获取天数即取上个月的最后一天的天数
+      var days = date.getDate();
+      return days;
+    },
+
+    //获取时间tab标签数据
+    //未来5天内
+    getBookedTabData: function() {
+      var week = "今天";
+      const date = new Date();
+      const cur_year = date.getFullYear();
+      const cur_month = date.getMonth() + 1;
+      const cur_date = date.getDate();
+      const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+      //利用构造函数创建对象tab数据对象
+      function calendar(date, week) {
+        this.date = cur_month + '-' + date;
+        if (date == cur_date) {
+          this.week = "今天";
+        } else if (date == cur_date + 1) {
+          this.week = "明天";
+        } else if (date == cur_date + 2) {
+          this.week = "后天";
+        } else {
+          this.week = '星期' + week;
+        }
+      }
+
+      var that = this;
+      //当前月份的天数
+      var monthLength = this.getThisMonthDays(cur_year, cur_month)
+      //当前月份的第一天是星期几
+      var firstWeek = this.getFirstDayOfWeek(cur_year, cur_month)
+      var x = firstWeek;
+      for (var i = 1; i <= monthLength; i++) {
+        //当循环完一周后，初始化再次循环
+        if (x > 6) {
+          x = 0;
+        }
+        //创建对象
+        that.data.calendar[i] = new calendar(i, [weeks_ch[x]][0])
+        x++;
+      }
+
+      //5天以内
+      var flag = that.data.calendar.splice(cur_date, that.data.calendar.length - cur_date <= 5 ? that.data.calendar.length : 5)
+      that.setData({
+        bookedTabData: flag
+      })
+      console.log(JSON.stringify(this.data.bookedTabData));
+    },
   }
 })
